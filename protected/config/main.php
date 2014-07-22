4e81aa48596bd8ca00d76186557cc4b41c45df77
spiritvoin <Spiritvoin88@gmail.com>
2014-07-22 11:02:35 +0300

    fsockopen error

---
+++ protected/config/main.php
@@ -0,0 +1,92 @@
+<?php
+
+// uncomment the following to define a path alias
+// Yii::setPathOfAlias('local','path/to/local-folder');
+
+// This is the main Web application configuration. Any writable
+// CWebApplication properties can be configured here.
+
+return array(
+	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+	'name'=>'Yii Blog Demo',
+
+	// preloading 'log' component
+	'preload'=>array('log'),
+
+	// autoloading model and component classes
+	'import'=>array(
+		'application.models.*',
+		'application.components.*',
+	),
+
+	'defaultController'=>'post',
+
+	// application components
+	'components'=>array(
+		'user'=>array(
+			// enable cookie-based authentication
+			'allowAutoLogin'=>true,
+		),
+//		'db'=>array(
+//			'connectionString' => 'sqlite:protected/data/blog.db',
+//			'tablePrefix' => 'tbl_',
+//		),
+		// uncomment the following to use a MySQL database
+
+		'db'=>array(
+			'connectionString' => 'pgsql:host=localhost;port=5432;dbname=yiisphinxsearch',
+			'emulatePrepare' => true,
+			'username' => 'postgres',
+			'password' => '123321',
+			'charset' => 'utf8',
+            'enableProfiling' => YII_DEBUG,
+            'enableParamLogging' => YII_DEBUG,
+            'schemaCachingDuration' => 3600,
+            'queryCacheID' => 'cache',
+		),
+
+		'errorHandler'=>array(
+			// use 'site/error' action to display errors
+			'errorAction'=>'site/error',
+		),
+		'urlManager'=>array(
+			'urlFormat'=>'path',
+			'rules'=>array(
+				'post/<id:\d+>/<title:.*?>'=>'post/view',
+				'posts/<tag:.*?>'=>'post/index',
+				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+			),
+		),
+		'log'=>array(
+			'class'=>'CLogRouter',
+			'routes'=>array(
+				array(
+					'class'=>'CFileLogRoute',
+					'levels'=>'error, warning',
+				),
+				// uncomment the following to show log messages on web pages
+				/*
+				array(
+					'class'=>'CWebLogRoute',
+				),
+				*/
+			),
+		),
+        'search' => array(
+            'class' => 'application.components.DGSphinxSearch',
+            'server' => '127.0.0.1',
+            'port' => 9312,
+            'maxQueryTime' => 3000,
+            'enableProfiling'=>0,
+            'enableResultTrace'=>0,
+            'fieldWeights' => array(
+                'name' => 10000,
+                'keywords' => 100,
+            ),
+        ),
+	),
+
+	// application-level parameters that can be accessed
+	// using Yii::app()->params['paramName']
+	'params'=>require(dirname(__FILE__).'/params.php'),
+);
